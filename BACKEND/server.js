e0991eb154c2c36bb2ba9272546bf9a6294d585c
VishwaChathuminaDeSilva <vishwa.chathumina.gaming@gmail.com>
2026-01-09 00:58:52 +0800

    Sever.js app.listen has been fixed

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -29,6 +29,6 @@ mongoose
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
